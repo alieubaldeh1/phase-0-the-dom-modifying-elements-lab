deeec9d925bb14d97dbf711b0a7b65ad82dbe2e6
Alieu Baldeh <jamtantech@gmail.com>
2021-06-15 07:58:00 -0400

    last lab completed!

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementsByTagName('main')[0];
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Alieu is the champion';
+newHeader.className = 'victory';
